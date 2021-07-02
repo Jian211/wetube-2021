abc4c912686c4d31affa2dc3b8480586a50abdb9
jian-level-is <myunggeun22@gmail.com>
2021-07-02 17:16:52 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,19 @@
 import express from "express";
 import morgan from "morgan";
-
-const PORT = 4000;
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 //creat application
 const app = express();
 
-const handleHome = (request , response) => {
-    console.log("im final");
-    return response.send("hi teaas");
-    
-}
-const handleProtected = (req, res) => {
-    return res.send("BOOOO YA~");
-}
-app.use(morgan("dev"));
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+const PORT = 4000;
 
+//middleWare
+app.use(morgan("dev"));
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 const handleListening = () => console.log(`Server listening on port ${PORT} BAAM~`);
 app.listen(PORT, handleListening);
