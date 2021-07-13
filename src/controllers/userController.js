38b43af52d2c50721c369e2b6238c960f31c8064
jian-level-is <myunggeun22@gmail.com>
2021-07-13 19:45:08 +0900

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
+
+
 export const join = (req, res) => res.send("Join user");
-export const edit = (req, res) => res.send("Edit User");
+export const edit = (req, res) => res.render("edit");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("login");
 export const logout = (req, res) => res.send("logout");
